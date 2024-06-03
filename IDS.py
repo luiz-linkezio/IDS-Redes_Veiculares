936ff0123ef985b5cf2ca42a9231602a3e008709
linkezio <henriqueb999@yahoo.com.br>
2024-06-02 23:45:52 -0300

    Criação de modelos LSTM

--- IDS.py
+++ IDS.py
@@ -1,61 +1,104 @@
 from joblib import load
+from keras.models import load_model
 from sklearn.svm import OneClassSVM
 import can
-import time
 import subprocess
 import numpy as np
 import pandas as pd
 
-def predict(model, message, model_name, timestamp):
-    global benign_count
-    global malicious_count
+def create_slicing_windows(data, labels=None, time_step=1):
+    X, Y = [], []
+    for i in range(len(data) - time_step):
+        a = data[i:(i + time_step)]
+
+        X.append(a)
+
+        if labels:
+            Y.append(labels[i + time_step])
+
+    return np.array(X), np.array(Y)
+
+def df_construct(messages, timestamp, scaler=None):
+
+    if isinstance(messages, list) == False:
+        messages = [messages]
 
     df_message = pd.DataFrame()
+    for message in messages:
 
-    df_message["Rel Time"] = [(message.timestamp - timestamp)]
-    df_message["DLC"] = [message.dlc]
-    df_message["Arb ID"] = [message.arbitration_id]
+        df_message["Rel Time"] = [(message.timestamp - timestamp)]
+        timestamp = message.timestamp
+        df_message["DLC"] = [message.dlc]
+        df_message["Arb ID"] = [message.arbitration_id]
 
-    counter = 0
-    for byte in message.data:
-        df_message[f'Byte {counter}'] = [byte]
-        counter += 1
-        
-    while counter < 4:
-        df_message[f'Byte {counter}'] = 0
-        counter += 1
-    
+        counter = 0
+        for byte in message.data:
+            df_message[f'Byte {counter}'] = [byte]
+            counter += 1
+            
+        while counter < 4:
+            df_message[f'Byte {counter}'] = -1
+            counter += 1
+
+    if scaler:
+        df_message_scaled = pd.DataFrame(scaler.transform(df_message), columns=df_message.columns, index=df_message.index)
+        return df_message_scaled
+    else:
+        return df_message
+
+def predict(message, timestamp, model, model_name, scaler=None, window_size=None):
+
+    df_message = df_construct(message, timestamp, scaler)
+
+    if window_size is not None:
+        message_window, _ = create_slicing_windows(df_message, None, window_size)
+
     if model_name == "OCSVM":
         predict = model.predict(df_message)
         
         if predict == 1:
-            benign_count += 1
             print(f"Mensagem BENIGNA: {message}")
         elif predict == -1:
-            malicious_count += 1
             print(f"Mensagem MALICIOSA: {message}")
-    
-    print(100*malicious_count/(benign_count+malicious_count),"%")
-    
-    return message.timestamp
 
-benign_count = 0
-malicious_count = 0
+
+    elif model_name == "LSTM":
+        predict = model.predict(message_window)
+
+        if predict > 0.5:
+            print(f"Janela BENIGNA de tamanho: {window_size}")
+        elif predict < 0.5:
+            print(f"Janela MALICIOSA de tamanho: {window_size}")
+
 
 ocsvm_model = load("/home/live/Documents/PET Luiz e Karen - Dupla 7/model_ocsvm.joblib")
 
+lstm_model = load_model("/home/live/Documents/PET Luiz e Karen - Dupla 7/xxxxxxxxxxxx")
+lstm_windows_size = 150
+lstm_scaler = load("/home/live/Documents/PET Luiz e Karen - Dupla 7/xxxxxxxxxxxxxx")
+
 with can.Bus(interface='socketcan', channel='can0', bitrate=500000) as bus:
 
-    messages = list()
+    count = 0
     timestamp = 0
+    lstm_messages_list = list()
     while True:
         try:
-            for message in bus:
-                timestamp = predict(ocsvm_model, message, "OCSVM", timestamp)
-                messages.append(message)
+            message = bus.recv()
+
+
+            predict(message, timestamp, ocsvm_model, "OCSVM")
+
+
+            lstm_messages_list.append(message)
+            count += 1
+            if count == lstm_windows_size:
+                predict(lstm_messages_list, timestamp, lstm_model, "LSTM", lstm_scaler, lstm_windows_size)
+
+            timestamp = message.timestamp
 
         except Exception as e:
-            print('erro', e)
+            print("Erro:", e)
             
             subprocess.run(["sudo", "ip", "link", "set", "can0", "down"])
             subprocess.run(["sudo", "ip", "link", "set", "can0", "up", "type", "can", "bitrate" , "500000"])
