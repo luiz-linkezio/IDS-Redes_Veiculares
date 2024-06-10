f3b9288000958c831d96ea4145e4d3e79520a400
linkezio <henriqueb999@yahoo.com.br>
2024-06-10 15:24:35 -0300

    Treinamento de novos modelos

--- IDS.py
+++ IDS.py
@@ -6,17 +6,17 @@ import subprocess
 import numpy as np
 import pandas as pd
 
-def create_slicing_windows(data, labels=None, time_step=1):
-    X, Y = [], []
-    for i in range(len(data) - time_step):
-        a = data[i:(i + time_step)]
+def gerar_lotes(dados, tamanho_lote):
+    for i in range(0, len(dados), tamanho_lote):
+        lote = dados[i:i + tamanho_lote]
+        yield lote
 
+def create_slicing_windows(data, time_step=1):
+    X = []
+    for c in range(1):
+        a = data[c:(c + time_step)]
         X.append(a)
-
-        if labels:
-            Y.append(labels[i + time_step])
-
-    return np.array(X), np.array(Y)
+    return np.array(X)
 
 def df_construct(messages, timestamp, scaler=None):
 
@@ -25,20 +25,19 @@ def df_construct(messages, timestamp, scaler=None):
 
     df_message = pd.DataFrame()
     for message in messages:
-
-        df_message["Rel Time"] = [(message.timestamp - timestamp)]
-        timestamp = message.timestamp
-        df_message["DLC"] = [message.dlc]
-        df_message["Arb ID"] = [message.arbitration_id]
-
+        
         counter = 0
+        byte_list = list()
         for byte in message.data:
-            df_message[f'Byte {counter}'] = [byte]
+            byte_list.append(byte)
             counter += 1
             
         while counter < 4:
-            df_message[f'Byte {counter}'] = -1
+            byte_list.append(-1)
             counter += 1
+        
+        df_message = df_message._append({'Timestamp': (message.timestamp - timestamp), 'DLC': message.dlc,'Arb ID': message.arbitration_id,'Byte 0': byte_list[0],'Byte 1':byte_list[1],'Byte 2':byte_list[2],'Byte 3':byte_list[3]}, ignore_index=True)
+        timestamp = message.timestamp
 
     if scaler:
         df_message_scaled = pd.DataFrame(scaler.transform(df_message), columns=df_message.columns, index=df_message.index)
@@ -47,37 +46,57 @@ def df_construct(messages, timestamp, scaler=None):
         return df_message
 
 def predict(message, timestamp, model, model_name, scaler=None, window_size=None, threshold=None):
+	global benign_count
+	global malicious_count
+	
+	df_message = df_construct(message, timestamp, scaler)
+	if window_size is not None:
+		message_window = create_slicing_windows(df_message, window_size)
+	
+	if model_name == "OCSVM":
+		predict = model.predict(df_message)
+		if predict == 1:
+			print(f"Mensagem BENIGNA: {message}")
+		elif predict == -1:
+			print(f"Mensagem MALICIOSA: {message}")
 
-    df_message = df_construct(message, timestamp, scaler)
+	elif model_name == "LSTMns":
+		predict = model.predict(message_window)
+		
+		reconstruction_error = np.mean(np.square(message_window - predict), axis=(1, 2))
+		
+		print(reconstruction_error)
+		if reconstruction_error < threshold:
+			print(f"Janela BENIGNA de tamanho: {window_size}")
+			benign_count += 1
+		elif reconstruction_error > threshold:
+			malicious_count += 1
+			print(f"Janela MALICIOSA de tamanho: {window_size}")
+		print(f"Benignas: {benign_count}, Maliciosas: {malicious_count}")
 
-    if window_size is not None:
-        message_window, _ = create_slicing_windows(df_message, None, window_size)
-
-    if model_name == "OCSVM":
-        predict = model.predict(df_message)
-        
-        if predict == 1:
-            print(f"Mensagem BENIGNA: {message}")
-        elif predict == -1:
-            print(f"Mensagem MALICIOSA: {message}")
-
-
-    elif model_name == "LSTM":
-        predict = model.predict(message_window)
-
-        if predict > threshold:
-            print(f"Janela BENIGNA de tamanho: {window_size}")
-        elif predict < threshold:
-            print(f"Janela MALICIOSA de tamanho: {window_size}")
+	elif model_name == "LSTMs":
+		predict = model.predict(message_window)
+		print(predict)
+		if predict > 0:
+			benign_count += 1
+			print(f"Janela BENIGNA de tamanho: {window_size}")
+		elif predict <= 0:
+			malicious_count += 1
+			print(f"Janela MALICIOSA de tamanho: {window_size}")
 
 
 #ocsvm_model = load("/home/live/Documents/PET Luiz e Karen - Dupla 7/model_ocsvm.joblib")
 
-lstm_model = load_model(" ")
+lstm_model = load_model("/home/petdupla7/Desktop/PET/models/model0_LSTM_ns_bc_ws150_t0,04415.keras")
+#lstm_model = load_model("/home/petdupla7/Desktop/PET/models/model0_LSTM_s_bc_ws150.keras")
 lstm_windows_size = 150
-lstm_scaler = load(" ")
-lstm_theshold = 0
+lstm_scaler = load("/home/petdupla7/Desktop/PET/models/scalers/scaler_model0_LSTM_ns_bc_ws150_t0,04415")
+#lstm_scaler = load("/home/petdupla7/Desktop/PET/models/scalers/scaler_model0_LSTM_s_bc_ws150")
+lstm_theshold = 0.12
+print(f"Threshold: {lstm_theshold}")
 
+benign_count = 0
+malicious_count = 0
 with can.Bus(interface='socketcan', channel='can0', bitrate=500000) as bus:
 
     count = 0
@@ -88,13 +107,15 @@ with can.Bus(interface='socketcan', channel='can0', bitrate=500000) as bus:
             message = bus.recv()
 
 
-            predict(message, timestamp, ocsvm_model, "OCSVM")
+            #predict(message, timestamp, ocsvm_model, "OCSVM")
 
 
             lstm_messages_list.append(message)
             count += 1
             if count == lstm_windows_size:
-                predict(lstm_messages_list, timestamp, lstm_model, "LSTM", lstm_scaler, lstm_windows_size, lstm_theshold)
+                predict(lstm_messages_list, timestamp, lstm_model, "LSTMns", lstm_scaler, lstm_windows_size, lstm_theshold)
+                count = 0
+                lstm_messages_list = list()
 
             timestamp = message.timestamp
 
